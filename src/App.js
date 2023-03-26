b0e442f25277b8f0a7f70998baffc16cf5879b28
JereLem <56977360+JereLem@users.noreply.github.com>
2023-03-26 13:55:23 +0300

    5.1-5.16 missing files added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,156 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
-import blogService from './services/blogs'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import blogService from './services/blogService'
+import loginService from './services/loginService'
+import { useField } from './hook'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
+  const [user, setUser] = useState(null)
+  const [update, setUpdate] = useState(null)
+  const password = useField('password')
+  const username = useField('text')
+  const title = useField('text')
+  const url = useField('text')
+  const author = useField('text')
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const loggedUser = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      blogService.getAll().then(blogs => setBlogs(blogs))
+    }
   }, [])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => setBlogs(blogs))
+  }, [update])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: username.object.value,
+        password: password.object.value
+      })
+
+      blogService.setToken(user.token)
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+
+      setUser(user)
+      username.reset()
+      password.reset()
+    } catch (exception) {
+      setMessage('wrong username or password')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    }
+  }
+
+  const handleLogout = async event => {
+    event.preventDefault()
+    try {
+      window.localStorage.removeItem('loggedBlogAppUser')
+      setUser(null)
+    } catch (exception) {
+      setMessage('Try again!')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    }
+  }
+
+  const addBlog = async event => {
+    event.preventDefault()
+    try {
+      const blogObject = {
+        user: user,
+        title: title.object.value,
+        author: author.object.value,
+        url: url.object.value
+      }
+      const auth = user.token
+      blogService.create(blogObject, auth).then(blog => {
+        setBlogs(blogs.concat(blog))
+        author.reset()
+        title.reset()
+        url.reset()
+        setMessage(`a new blog with the title: ${blogObject.title} by ${blogObject.author} added`)
+      })
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    } catch (exception) {
+      setMessage('Blog not added')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin} className="login">
+      <h2>Log in to the blog application:</h2>
+      <div>
+        Username:
+        <input {...username.object} />
+      </div>
+      <div>
+        Password:
+        <input {...password.object} />
+      </div>
+      <button type="submit">Log in</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <div>
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => (
+          <Blog key={blog.id} blog={blog} setUpdate={setUpdate} user={user} />
+        ))}
+    </div>
+  )
+
+  const newBlogFrom = () => (
+    <form onSubmit={addBlog}>
+      <div> <h2>Create new blog</h2></div>
+      <div>
+        Title:
+        <input {...title.object} />
+      </div>
+      <div>
+        Author:
+        <input {...author.object} />
+      </div>
+      <div>
+        Url:
+        <input {...url.object} />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification message={message} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <p>{user.username} logged in</p>
+          <button onClick={handleLogout}> logout</button>
+          {blogForm()}
+          <br />
+          <Togglable buttonLabel="Create New Blog">{newBlogFrom()}</Togglable>
+        </div>
       )}
     </div>
   )
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,67 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+import blogService from '../services/blogService'
+
+const blogStyles = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+}
+
+const Blog = ({ blog, onUpdate, user }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [isRemovable, setIsRemovable] = useState(false)
+
+  const hiddenStyle = { display: isVisible ? 'none' : '' }
+  const visibleStyle = { display: isVisible ? '' : 'none' }
+  const removableStyle = { display: isRemovable ? '' : 'none' }
+
+  const handleClick = () => {
+    setIsVisible(true)
+    if (blog.user.username !== user.username) {
+      setIsRemovable(true)
+    }
+  }
+
+  const handleLike = async event => {
+    event.preventDefault()
+    const updatedBlog = { ...blog, likes: blog.likes + 1 }
+    await blogService.update(blog.id, updatedBlog)
+    onUpdate(Math.floor(Math.random() * 100))
+  }
+
+  const handleRemove = async event => {
+    event.preventDefault()
+    if (window.confirm(`Remove blog with title ${blog.title} by ${blog.author}`)) {
+      blogService.setToken(user.token)
+      await blogService.remove(blog.id)
+      onUpdate(Math.floor(Math.random() * 100))
+    }
+  }
+
+  return (
+    <div style={blogStyles} className="blog">
+      <div style={hiddenStyle}>
+        <div onClick={handleClick} className="title-author">
+          {blog.title} {blog.author}
+        </div>
+      </div>
+      <div style={visibleStyle} className="details">
+        <div className="title">{blog.title}</div>
+        <div className="url">{blog.url}</div>
+        <div className="likes">
+          {blog.likes} likes{' '}
+          <button type="button" onClick={handleLike}>Like
+          </button>
+        </div>
+        <div style={removableStyle} className="author">added by {blog.author}
+          <button type="button" onClick={handleRemove}>Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
